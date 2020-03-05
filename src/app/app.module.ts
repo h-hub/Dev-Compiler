b3800f0b8cc2812bf3c36c5e0b06a467b07f9650
h-hub <harsha.kj89@gmail.com>
2020-03-05 16:30:26 +0800

    add home and tools component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ToolsComponent } from './tools/tools.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'tools', component: ToolsComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/tools/tools.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tools',
+  templateUrl: './tools.component.html',
+  styleUrls: ['./tools.component.css']
+})
+export class ToolsComponent implements OnInit {
+
+  number = 123;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ToBinary (value) {
+    return Number(value).toString(2);
+  }
+
+}
